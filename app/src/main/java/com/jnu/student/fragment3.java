4db847f3d8a82ef78ac33ca4d429bfe52d6a175f
MyFirstApplication <a82954531@gmail.com>
2024-01-01 21:45:44 +0800

    Fragment

--- app/src/main/java/com/jnu/student/fragment3.java
+++ app/src/main/java/com/jnu/student/fragment3.java
@@ -1,4 +1,18 @@
 package com.jnu.student;
 
-public class fragment3 {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class fragment4 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.right_fragment, container, false);
+        // Perform any additional setup for the fragment here
+        return view;
+    }
 }
